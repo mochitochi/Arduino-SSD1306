bb9df8f5e4052d9f5418a561deac28cd5cd2ba72
masa2004 <masa2004.jpn@gmail.com>
2020-08-01 14:58:36 +0900

    Hello,World!

--- Arduino-SSD1306.ino
+++ Arduino-SSD1306.ino
@@ -1,6 +1,8 @@
 #include <Wire.h>
-#include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+
+void printtext(int,int,int,int,char);
 
 Adafruit_SSD1306 display(-1);
 
@@ -11,5 +13,19 @@ void setup() {
 void loop(){
     display.clearDisplay();
 
+    /*display.setTextSize(3);
+    display.setCursor(0,0);
+    display.setTextColor(WHITE);
+    display.println("Hello,World!");*/
+
+    printtext(1,x,0,WHITE,"Hello,World!");
     display.display();
+    delay(25);
+}
+
+void printtext(int size,int x,int y,int color,char text[20]){
+    display.setTextColor(color);
+    display.setTextSize(size);
+    display.setCursor(x,y);
+    display.println(text);
 }
